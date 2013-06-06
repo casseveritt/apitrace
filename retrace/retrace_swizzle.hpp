1cb2c35b8847cc4afd0aca628df31ea80cd7b1e9
Cass Everitt <cass@xyzw.us>
2013-06-06 09:32:26 -0500

    hacks to get most things rendering on Linux

--- retrace/retrace_swizzle.hpp
+++ retrace/retrace_swizzle.hpp
@@ -74,6 +74,28 @@ public:
         }
         return it->second;
     }
+
+    T & lookupUniformLocation( const T &key ) {
+      typename base_type::iterator it;
+      it = base.find(key);
+      if (it != base.end()) {
+        return it->second;
+      }
+
+      // otherwise add it by finding the nearest mapped slot number and populating the map
+      int baseKey = key;
+      while( baseKey > 0 ) {
+        it = base.find(baseKey);
+        if (it != base.end()) {
+          break;
+        }
+        baseKey--;
+      }
+      for( int k = baseKey+1; k <= key; k++ ) {
+        base[k] = base[k-1]+1;
+      }
+      return base[key];
+    }
 };
 
 
